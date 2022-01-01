57cd8eb661afa5e2429f9d212a254a94738fe7db
Kieran Healy <kjhealy@gmail.com>
2022-01-01 14:34:16 -0500

    Fix menu issue

--- local_git_config.sh
+++ local_git_config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git config --local user.name "zwbetz-gh"
-git config --local user.email "zwbetz@gmail.com"
+git config --local user.name "kjhealy"
+git config --local user.email "kjhealy@gmail.com"
 
 git config --local --list
